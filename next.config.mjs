f8af76a746ffa4addbb689123d6939c0ef694749
An Pham <ancs21.ps@gmail.com>
2023-05-02 15:31:29 +0700

    🔥 chore: remove unused code-hike remark plugin and shiki theme import 🚀 feat: add react-icons dependency to package.json 🔧 chore: update tailwind.config.js to include only files in src/ directory for content scanning

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,19 @@
-import { remarkCodeHike } from '@code-hike/mdx'
+/**
+ * @type {import('next').NextConfig}
+ */
+// import { remarkCodeHike } from '@code-hike/mdx'
 import nextra from 'nextra'
 import { remarkMermaid } from 'remark-mermaid-nextra'
-import theme from 'shiki/themes/material-theme.json' assert { type: 'json' }
+// import theme from 'shiki/themes/material-theme.json' assert { type: 'json' }
 
 const withNextra = nextra({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
   mdxOptions: {
-    remarkPlugins: [remarkMermaid, [remarkCodeHike, { theme }]],
+    remarkPlugins: [remarkMermaid],
   },
+  codeHighlight: true,
+  defaultShowCopyCode: true,
 })
 
 export default withNextra()
